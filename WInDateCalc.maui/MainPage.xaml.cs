1d95a2e3ea1e051b36f55210ca2025ae73d88d28
Giulio Sorrentino <gsorre84@gmail.com>
2023-02-10 09:38:35 +0100

    internazionalizzato

--- WInDateCalc.maui/MainPage.xaml.cs
+++ WInDateCalc.maui/MainPage.xaml.cs
@@ -1,15 +1,8 @@
-﻿using System.Net.NetworkInformation;
-using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
-using Application = Microsoft.Maui.Controls.Application;
-
-namespace WInDateCalc.maui;
+﻿namespace WInDateCalc.maui;
 public partial class MainPage : ContentPage
 {
-    int count = 0;
-
     public MainPage()
     {
-        String s;
         InitializeComponent();
         while (true)
             try
@@ -21,19 +14,53 @@ public partial class MainPage : ContentPage
             {
                 Preferences.Clear();
             }
+#if ANDROID
+        ldate.Text = GetResource(WinDateCalc.maui.Resource.String.insert_the_date);
+        bok.Text=GetResource(WinDateCalc.maui.Resource.String.calculate);
+        binfo.Text=GetResource(WinDateCalc.maui.Resource.String.informations);
+#elif NET7_0_OR_GREATER
+#endif
+    }
+
+    private string GetResource(object insert_the_date)
+    {
+        throw new NotImplementedException();
     }
 
     private void calcola_Click(object sender, EventArgs e)
     {
         DateTime d = DateTime.Now;
-        TimeSpan differenza = data.Date.Date - d;
-        risultato.Text = $"Mancano {differenza.Days} giorni.";
-        Preferences.Set("Data", data.Date.Date.ToString());
+        TimeSpan differenza = data.Date - d;
+        if (differenza.TotalMilliseconds < 0) {
+#if ANDROID
+            risultato.Text = GetResource(WinDateCalc.maui.Resource.String.invalid_lvalue);
+#elif NET7_0_OR_GREATER
+#endif
+            return;
+        }
+#if ANDROID
+    risultato.Text = $"{GetResource(WinDateCalc.maui.Resource.String.there_are)} {Math.Ceiling(differenza.TotalDays)} {GetResource(WinDateCalc.maui.Resource.String.days_left)}.";
+#elif NET7_0_OR_GREATER
+#endif
+        Preferences.Set("Data", data.Date.ToString());
     }
 
     private async void info_Click(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new InfoPage());
     }
+
+#if ANDROID
+    private System.String GetResource(int id)
+    {
+        return Android.App.Application.Context.Resources.GetString(id);
+
+    }
+#elif NET7_0_OR_GREATER
+    private System.String GetResource(string id)
+    {
+        return Resources[id].ToString();
+    }
+#endif
 }
 
